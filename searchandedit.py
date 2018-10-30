7a49957a6baf510baf97c36f40218d12ef6a64c4
bados <bambam_conan1@live.com>
2018-10-30 11:14:51 +0700

    Create searchandedit.py

---
+++ searchandedit.py
@@ -0,0 +1,29 @@
+import xlwings as xw
+import string
+from xlwings.constants import DeleteShiftDirection,InsertShiftDirection
+###################################################################
+
+bookName = r'C:\Users\BaboBaboo\Desktop\pro\test.xlsx'  #local file
+wb = xw.Book(bookName)
+sheetName = 'Sheet1'  #sheet name
+sht = wb.sheets[sheetName]
+
+###################################################################
+print ('""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""')
+print ('""""""""""""""""""""""""///  EDIT  EXCEL  ///""""""""""""""""""""""" :')
+print ('""""""""""""""""""""""""""""""""""""""""""""""""""""""'' babobaboo""""')
+print('Keyword Search :')
+word = input()
+print('word Edit:')
+wordd = input()
+###################################################################
+search = word #search word to edit
+
+myCell = wb.sheets[sheetName].api.UsedRange.Find(search)
+A = myCell.address.replace('$', '', 1)
+A = myCell.address.replace('$', '', 2)
+print ('Address  :'+A)
+print ('=== success ===')
+sht.range(A).value = wordd #new word edit 
+
+
